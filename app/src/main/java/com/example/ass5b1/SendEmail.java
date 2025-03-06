b2006946993e03540b359103bacd8a1edf6d4d25
ron-s-k <ronitk8484@gmail.com>
2025-03-06 21:55:13 +0530

    Initial Commit

--- app/src/main/java/com/example/ass5b1/SendEmail.java
+++ app/src/main/java/com/example/ass5b1/SendEmail.java
@@ -3,7 +3,6 @@ package com.example.ass5b1;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
